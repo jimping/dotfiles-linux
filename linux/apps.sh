29a4e42fe588839a79dbe73bb93e2331e138feb3
Jonas Imping <j.imping@splintnet.de>
2022-08-31 17:05:35 +0200

    invert

--- linux/apps.sh
+++ linux/apps.sh
@@ -1,6 +1,6 @@
 sudo apt-get install terminator
 
-if ! command -v flatpak &> /dev/null
+if command -v flatpak &> /dev/null
 then
     flatpak install slack --or-update --noninteractive
     flatpak install thunderbird --or-update --noninteractive
