1027d913f17745e5cdda952f8c4f05df44baef2f
Jonas Imping <j.imping@splintnet.de>
2022-09-01 10:08:18 +0200

    apps

--- linux/apps.sh
+++ linux/apps.sh
@@ -1,9 +1,15 @@
+# Terminator
 sudo apt-get install terminator
 
+# Flatpak Apps
+# Slack
+# Thunderbird
+# Audacity
 if command -v flatpak > /dev/null; then
     flatpak install slack --or-update --noninteractive
     flatpak install thunderbird --or-update --noninteractive
     flatpak install audacity --or-update --noninteractive
 fi
 
-wget -O $HOME/BabelEdit-3.0.1.deb https://www.codeandweb.com/download/babeledit/3.0.1/BabelEdit-3.0.1.deb && sudo dpkg -i $HOME/BabelEdit-3.0.1.deb && rm -rf $HOME/BabelEdit-3.0.1.deb
+# BabelEdit
+sudo apt-get install -y libopengl0 && wget -O $HOME/BabelEdit-3.0.1.deb https://www.codeandweb.com/download/babeledit/3.0.1/BabelEdit-3.0.1.deb && sudo dpkg -i $HOME/BabelEdit-3.0.1.deb && rm -rf $HOME/BabelEdit-3.0.1.deb
