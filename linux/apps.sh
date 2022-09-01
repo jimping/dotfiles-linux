4d009ad68e08f259fd60a030208752cb73a4fdbc
Jonas Imping <j.imping@splintnet.de>
2022-09-01 10:01:23 +0200

    alias

--- linux/apps.sh
+++ linux/apps.sh
@@ -5,3 +5,5 @@ if command -v flatpak > /dev/null; then
     flatpak install thunderbird --or-update --noninteractive
     flatpak install audacity --or-update --noninteractive
 fi
+
+wget -O $HOME/BabelEdit-3.0.1.deb https://www.codeandweb.com/download/babeledit/3.0.1/BabelEdit-3.0.1.deb && sudo dpkg -i $HOME/BabelEdit-3.0.1.deb && rm -rf $HOME/BabelEdit-3.0.1.deb
